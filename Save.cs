041bcad88575c98a8e7903863079401ca0396bbd
lordBruticus <crash876@gmail.com>
2020-03-31 13:54:50 -0600

    Add files via upload

---
+++ Save.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Save
+{
+    public List<int> livingTargetPositions = new List<int>();
+    public List<int> livingTargetsTypes = new List<int>();
+
+    public int waves = 0;
+    public int kills = 0;
+}
